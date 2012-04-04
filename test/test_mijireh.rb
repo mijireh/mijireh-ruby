af8eb389713fb1374b3c41123e7943aa358102b9
Joey Beninghove <joeybeninghove@gmail.com>
2012-04-04 00:16:30 -0400

    Cleaned up spacing because it bugged me

--- test/test_mijireh.rb
+++ test/test_mijireh.rb
@@ -2,10 +2,8 @@ require 'test/unit'
 require 'mijireh'
 
 class MijirehTest < Test::Unit::TestCase
-
   def test_access_key
     Mijireh.access_key = 'blah'
     assert_equal 'blah', Mijireh.access_key
   end
-
 end
